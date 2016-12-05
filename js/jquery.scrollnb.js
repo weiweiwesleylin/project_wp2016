762ef2ebf2d0839db94a5dfcf906dbc66da93288
weiweiwesleylin <dan840311@gmail.com>
2016-12-05 15:52:43 +0800

    total

--- js/jquery.scrollnb.js
+++ js/jquery.scrollnb.js
@@ -8,7 +8,55 @@ $(document).ready(function() {
   // add another event handler
 });
 
+$(document).ready(function() {
+  $('#kgs2').on('keyup', function() {
+    var kgs2 = +$(this).val();
+    var dailyPrice2 = +$(this).closest(".fruit2").data("daily-price2");
+    $('#total2').text(kgs2 * dailyPrice2);
+    $('#kg2').text($(this).val());
+  });
+  // add another event handler
+});
 
+$(document).ready(function() {
+  $('#kgs3').on('keyup', function() {
+    var kgs3 = +$(this).val();
+    var dailyPrice3 = +$(this).closest(".fruit3").data("daily-price3");
+    $('#total3').text(kgs3 * dailyPrice3);
+    $('#kg3').text($(this).val());
+  });
+  // add another event handler
+});
+
+$(document).ready(function() {
+  $('#kgs4').on('keyup', function() {
+    var kgs4 = +$(this).val();
+    var dailyPrice4 = +$(this).closest(".fruit4").data("daily-price4");
+    $('#total4').text(kgs4 * dailyPrice4);
+    $('#kg4').text($(this).val());
+  });
+  // add another event handler
+});
+
+$(document).ready(function() {
+  $('#kgs5').on('keyup', function() {
+    var kgs5 = +$(this).val();
+    var dailyPrice5 = +$(this).closest(".fruit5").data("daily-price5");
+    $('#total5').text(kgs5 * dailyPrice5);
+    $('#kg5').text($(this).val());
+  });
+  // add another event handler
+});
+
+$(document).ready(function() {
+  $('#kgs6').on('keyup', function() {
+    var kgs6 = +$(this).val();
+    var dailyPrice6 = +$(this).closest(".fruit6").data("daily-price6");
+    $('#total6').text(kgs6 * dailyPrice6);
+    $('#kg6').text($(this).val());
+  });
+  // add another event handler
+});
 /*$(document).ready(function(){
   $('#mango').click(function(){
     $('.ui.modal').modal('show');
