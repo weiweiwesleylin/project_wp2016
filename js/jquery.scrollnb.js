242e65a14f713b1d3458c2dbfda4ddf9ec9770f9
weiweiwesleylin <dan840311@gmail.com>
2016-12-05 15:59:44 +0800

    fruit

--- js/jquery.scrollnb.js
+++ js/jquery.scrollnb.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#kgs1').on('keyup', function() {
     var kgs1 = +$(this).val();
-    var dailyPrice1 = +$(this).closest(".fruit1").data("daily-price1");
+    var dailyPrice1 = +$(this).closest(".fruit").data("daily-price1");
     $('#total1').text(kgs1 * dailyPrice1);
     $('#kg1').text($(this).val());
   });
@@ -11,51 +11,46 @@ $(document).ready(function() {
 $(document).ready(function() {
   $('#kgs2').on('keyup', function() {
     var kgs2 = +$(this).val();
-    var dailyPrice2 = +$(this).closest(".fruit2").data("daily-price2");
+    var dailyPrice2 = +$(this).closest(".fruit").data("daily-price2");
     $('#total2').text(kgs2 * dailyPrice2);
     $('#kg2').text($(this).val());
   });
-  // add another event handler
 });
 
 $(document).ready(function() {
   $('#kgs3').on('keyup', function() {
     var kgs3 = +$(this).val();
-    var dailyPrice3 = +$(this).closest(".fruit3").data("daily-price3");
+    var dailyPrice3 = +$(this).closest(".fruit").data("daily-price3");
     $('#total3').text(kgs3 * dailyPrice3);
     $('#kg3').text($(this).val());
   });
-  // add another event handler
 });
 
 $(document).ready(function() {
   $('#kgs4').on('keyup', function() {
     var kgs4 = +$(this).val();
-    var dailyPrice4 = +$(this).closest(".fruit4").data("daily-price4");
+    var dailyPrice4 = +$(this).closest(".fruit").data("daily-price4");
     $('#total4').text(kgs4 * dailyPrice4);
     $('#kg4').text($(this).val());
   });
-  // add another event handler
 });
 
 $(document).ready(function() {
   $('#kgs5').on('keyup', function() {
     var kgs5 = +$(this).val();
-    var dailyPrice5 = +$(this).closest(".fruit5").data("daily-price5");
+    var dailyPrice5 = +$(this).closest(".fruit").data("daily-price5");
     $('#total5').text(kgs5 * dailyPrice5);
     $('#kg5').text($(this).val());
   });
-  // add another event handler
 });
 
 $(document).ready(function() {
   $('#kgs6').on('keyup', function() {
     var kgs6 = +$(this).val();
-    var dailyPrice6 = +$(this).closest(".fruit6").data("daily-price6");
+    var dailyPrice6 = +$(this).closest(".fruit").data("daily-price6");
     $('#total6').text(kgs6 * dailyPrice6);
     $('#kg6').text($(this).val());
   });
-  // add another event handler
 });
 /*$(document).ready(function(){
   $('#mango').click(function(){
